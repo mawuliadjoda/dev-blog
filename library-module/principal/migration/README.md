6a5e20a3bc7394101de1b25f003d8fe33cd3879b
Mawuli ADJODA <koffimawuli.adjoda@gmail.com>
2025-09-06 13:26:28 +0200

    Fix liquibase migration Dockerfile

---
+++ library-module/principal/migration/README.md
@@ -0,0 +1,15 @@
+
+cd principal
+docker build -t adjodamawuli/my_migration:1.0.0 -f migration/Dockerfile .
+
+docker run --rm adjodamawuli/my_migration:1.0.0
+
+
+
+# docker run --rm \
+# -e LIQUIBASE_COMMAND=update \
+# -e LIQUIBASE_LOG_LEVEL=info \
+# -e LIQUIBASE_COMMAND_URL=jdbc:postgresql://postgres:5432/person \
+# -e LIQUIBASE_COMMAND_USERNAME=root \
+# -e LIQUIBASE_COMMAND_PASSWORD=root123 \
+# adjodamawuli/my_migration:1.0.0
---
+++ library-module/principal/migration/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#export LIQUIBASE_COMMAND_URL="jdbc:postgresql://postgres:5432/person"
+export LIQUIBASE_COMMAND_URL="jdbc:postgresql://host.docker.internal:5432/postgres"
+#export LIQUIBASE_COMMAND_URL="jdbc:postgresql://db:5432/person"
+
+export LIQUIBASE_COMMAND_USERNAME="root"
+export LIQUIBASE_COMMAND_PASSWORD="root123"
+
+export LIQUIBASE_DEFAULT_SCHEMA_NAME="public"
+#export LIQUIBASE_DEFAULT_SCHEMA_NAME="person_schema"
+
+export LIQUIBASE_COMMAND_CHANGELOG_FILE="db/changelog/db.changelog-master.yml"
+#export LIQUIBASE_COMMAND_CHANGELOG_FILE="db/changelog/rbac/db/changelog/rbac/db.changelog-master.yml"
+
+liquibase update
