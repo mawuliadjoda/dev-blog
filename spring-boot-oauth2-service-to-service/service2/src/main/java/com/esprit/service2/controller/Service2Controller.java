406b584d506a7f0af4149e053d9c0f08d3500f37
Mawulli Adjoda <57372553+mawuliadjoda@users.noreply.github.com>
2025-11-22 15:29:34 +0100

    spring-boot-oauth2-service-to-service

--- spring-boot-oauth2-service-to-service/service2/src/main/java/com/esprit/service2/controller/Service2Controller.java
+++ spring-boot-oauth2-service-to-service/service2/src/main/java/com/esprit/service2/controller/Service2Controller.java
@@ -1,6 +1,7 @@
 package com.esprit.service2.controller;
 
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class Service2Controller {
 
+    @PreAuthorize("hasAnyAuthority('SCOPE_service2:resource1', 'SCOPE_service2:resource2')")
     @GetMapping("/hello")
     public String hello(@AuthenticationPrincipal Jwt jwt) {
         return "Hello from B, sub=" + jwt.getSubject();
